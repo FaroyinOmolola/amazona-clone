88e195845a5649160aa6d156ab074eec1f81b1a2
seunexplicit <seunxplicit@gmail>
2021-08-25 17:39:24 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,72 @@
+const data = {
+	products:[
+		{
+			id: "1",
+			name: "Dog Can Food",
+			category: "dog food",
+			image: "/images/233-2331886_canned-pedigree-dog-food-hd-png-download.png",
+			price: 1200,
+			brand: "Pedigree",
+			rating: 4.5,
+			numReview: 10,
+			description: "Quality food ration for all dog age group"
+		},
+		{
+			id: "2",
+			name: "Dog Dry Food",
+			category: "dog food",
+			image: "/images/233-2331886_canned-pedigree-dog-food-hd-png-download.png",
+			price: 800,
+			brand: "Pedigree",
+			rating: 4.0,
+			numReview: 7,
+			description: "Quality dog food for parseCustomProperties(parseCustomProperties)"
+		},
+		{
+			id: "1",
+			name: "Dog leash",
+			category: "dog accesories",
+			image: "/images/233-2331886_canned-pedigree-dog-food-hd-png-download.png",
+			price: 3500,
+			brand: "Korex",
+			rating: 5.0,
+			numReview: 12,
+			description: "Quality and strong leash for giant dogs"
+		},
+		{
+			id: "1",
+			name: "Dog shampoo",
+			category: "dog care products",
+			image: "/images/233-2331886_canned-pedigree-dog-food-hd-png-download.png",
+			price: 1500,
+			brand: "Animal care",
+			rating: 4.5,
+			numReview: 6,
+			description: "Quality food ration for all dog age group"
+		},
+		{
+			id: "1",
+			name: "Dog Can Food",
+			category: "dog food",
+			image: "/images/233-2331886_canned-pedigree-dog-food-hd-png-download.png",
+			price: 1200,
+			brand: "Pedigree",
+			rating: 4.5,
+			numReview: 10,
+			description: "Quality food ration for all dog age group"
+		},
+		{
+			id: "1",
+			name: "Dog Can Food",
+			category: "dog food",
+			image: "/images/233-2331886_canned-pedigree-dog-food-hd-png-download.png",
+			price: 1200,
+			brand: "Pedigree",
+			rating: 4.5,
+			numReview: 10,
+			description: "Quality food ration for all dog age group"
+		},
+	],
+};
+
+export default data
